48796b9c46e58a57c97f955ec37a67ff2fd5142f
s-gogna <s-gogna@users.noreply.github.com>
2015-09-24 20:25:22 -0700

    Added InsertionSort

---
+++ InsertionSort.cpp
@@ -0,0 +1,23 @@
+/*!****************************************************************************
+@file InsertionSort.cpp
+
+Case     | Time Complexity
+-------------------------------------------------------------------------------
+Best     |  O(n) comparisons, O(1) swaps
+Average  |  O(n^2) comparisons and swaps
+Worst    |  O(n^2) comparisons and swaps
+******************************************************************************/
+
+template <typename T>
+void InsertionSort(vector<T>& input)
+{
+   // For x in [0 to length-2]
+
+      // y is initialized to x
+
+      // While y > 0 and input[y] > input[y+1]
+
+         // Swap
+
+         // Decrement y
+}
