b4704beee28719644ad6aabc5f9f1ec2001a9a3c
s-gogna <s-gogna@users.noreply.github.com>
2015-09-24 20:25:31 -0700

    Added SelectionSort

---
+++ SelectionSort.cpp
@@ -0,0 +1,27 @@
+/*!****************************************************************************
+@file SelectionSort.cpp
+
+Case     | Time Complexity
+-------------------------------------------------------------------------------
+Best     |  O(n^2)
+Average  |  O(n^2)
+Worst    |  O(n^2)
+******************************************************************************/
+
+template <typename T>
+void SelectionSort(vector<T>& input)
+{
+	// For x in [0 to length-2]
+
+		// Initialize minIndex to x
+
+		// For y in [x to length-1]
+
+			// If input[x] > input[y]
+
+				// MinIndex is assigned y
+
+		// If minIndex is not equal to x
+
+			// Swap
+}
