32c3dac66e491890047b924279469a692f82bf52
s-gogna <s-gogna@users.noreply.github.com>
2015-09-24 19:49:21 -0700

    Added BubbleSort

---
+++ BubbleSort.cpp
@@ -0,0 +1,33 @@
+/*!*****************************************************************************
+@file BubbleSort.cpp
+
+Case     | Time Complexity
+--------------------------
+Best     |  O(n)
+Average  |  O(n^2)
+Worst    |  O(n^2)
+*******************************************************************************/
+
+template <typename T>
+void BubbleSort(vector<T>& input)
+{
+   // Initialize swapped to true
+
+   // Initialize n to length of input
+
+   // While swapped
+
+      // Set swapped to false
+
+      // For x in [0 to n-2]
+
+         // If input[x] > input[x+1]
+
+            // Swap
+
+            // Set swapped to true
+
+      // n = n - 1
+      // This is done a minor optimization since the last element
+      // in the sub-sequence is known to be the largest
+}
