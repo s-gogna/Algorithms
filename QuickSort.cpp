56f75132463922b71068b8c97121fd42af74a919
s-gogna <s-gogna@users.noreply.github.com>
2015-09-26 00:07:22 -0700

    Add QuickSort

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,15 +1,54 @@
 /*!****************************************************************************
 @file QuickSort.cpp
 
-Case     | Time Complexity
+Case           |  Complexity
 -------------------------------------------------------------------------------
-Best     |
-Average  |
-Worst    |
+Best           |  O(n log n)
+Average        |  O(n log n)
+Worst          |  O(n^2)
+-------------------------------------------------------------------------------
+Space (Worst)  |  O(n) total   O(n) auxiliary
+
+Space (Worst, auxiliary) can be reduced to O(log n) by performing quicksort
+recursively on the smaller partition and using tail recusion to quicksort
+the larger partition.
 ******************************************************************************/
 
 template <typename T>
-void QuickSort(vector<T>& input)
+void QuickSort(vector<T>& input, size_t low, size_t high)
 {
-   // TODO
+   // If low < high
+   // (ignores empty vectors since they are already sorted)
+   // (ignores single element vectors since they are already sorted)
+   // (ignores inverted low and high values)
+
+      // Compute the partition index
+      // p = partition(input, low, high)
+
+      // QuickSort(input, low, p)
+      // QuickSort(input, p + 1, high)
+}
+
+size_t partition(vector<T>& input, size_t low, size_t high)
+{
+   // Pivot is set to input[low]
+
+   // Index a is initialized to low
+   // Index b is initialized to high
+
+   // While a < b
+
+      // While input[a] < pivot
+
+         // a = a + 1
+
+      // While input[b] > pivot
+
+         // b = b - 1
+
+      // If i < j
+
+         // Swap input[i] and input[j]
+
+   // Return j
 }
